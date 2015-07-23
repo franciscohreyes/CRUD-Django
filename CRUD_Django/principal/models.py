4907dc6a3df8e3482579aab77d35104f9ed3e1dd
franciscohernandez <francisco.hdezreyes@gmail.com>
2015-07-23 16:39:03 -0500

    modificaciones en el archivo base, crud de los catalogos terminado

--- CRUD_Django/principal/models.py
+++ CRUD_Django/principal/models.py
@@ -12,7 +12,6 @@ REDES_SOCIALES = (
 class Agenda(models.Model):
 	nombre = models.CharField(max_length=50)
 	apellidos = models.CharField(max_length=100)
-	direccion = models.CharField(max_length=100)
 	telefono = models.CharField(max_length=100)
 	correo = models.EmailField(max_length=100)
 	redes_sociales = models.CharField(max_length=50, choices=REDES_SOCIALES)
@@ -21,3 +20,24 @@ class Agenda(models.Model):
 
 	def __str__(self):
 		return '%s - %s - %s' % (self.nombre, self.apellidos, self.cat_estatus.descripcion)
+
+
+TIPO_DIRECCION = (
+	('Andador', 'ANDADOR'),
+	('Av', 'AVENIDA'),
+	('Cda', 'CERRADA')
+)
+
+
+class Direcciones(models.Model):
+	nombre = models.CharField(max_length=50)
+	ciudad = models.CharField(max_length=50)
+	calle = models.CharField(max_length=100)
+	codigo_postal = models.CharField(max_length=100)
+	tipo_direccion = models.CharField(max_length=50, choices=TIPO_DIRECCION, default='')
+	fecha_registro = models.DateField(auto_now_add=True)
+	agenda = models.ForeignKey(Agenda)
+	cat_estatus = models.ForeignKey(Cat_estatus)
+
+	def __str__(self):
+		return '%s - %s - %s' % (self.nombre, self.ciudad, self.cat_estatus.descripcion)
--- CRUD_Django/principal/views.py
+++ CRUD_Django/principal/views.py
@@ -1,16 +1,29 @@
 # encoding:utf-8
 from django.contrib import messages
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.db import transaction
 from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
-from principal.forms import CrearContactoForm, EditarContactoForm, EliminarContactoForm
-from principal.models import Agenda
+from principal.forms import CrearContactoForm, EditarContactoForm, EliminarContactoForm, CrearDireccionContactoForm, EditarDireccionContactoForm
+from principal.models import Agenda, Direcciones
+from catalogos.models import Cat_estatus
 
 
 class Inicio(TemplateView):
 	template_name = "index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(Inicio, self).get_context_data(**kwargs)
+		contactos_registrados = Agenda.objects.filter(cat_estatus__descripcion="ACTIVO").count()
+		contactos_eliminados = Agenda.objects.filter(cat_estatus__descripcion="INACTIVO").count()
+		direcciones_registradas = Direcciones.objects.filter(cat_estatus__descripcion="ACTIVO").count()
+		estatus_registrados = Cat_estatus.objects.all().count()
+		context['contactos_registrados'] = contactos_registrados
+		context['contactos_eliminados'] = contactos_eliminados
+		context['direcciones_registradas'] = direcciones_registradas
+		context['estatus_registrados'] = estatus_registrados
+		return context
 
 
 class ListaContactosView(ListView):
@@ -28,7 +41,7 @@ class ListaContactosView(ListView):
 
 	def get_context_data(self, **kwargs):
 		context = super(ListaContactosView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		context['contacto'] = 'open'
 		return context
 
 
@@ -47,7 +60,7 @@ class ListaContactosInactivosView(ListView):
 
 	def get_context_data(self, **kwargs):
 		context = super(ListaContactosInactivosView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		context['contacto'] = 'open'
 		return context
 
 
@@ -65,7 +78,7 @@ class AgregarContactoView(CreateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(AgregarContactoView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		context['contacto'] = 'open'
 		return context
 
 
@@ -75,7 +88,9 @@ class DetalleContactoView(DetailView):
 
 	def get_context_data(self, **kwargs):
 		context = super(DetalleContactoView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		direcciones = Direcciones.objects.filter(agenda_id=self.kwargs["pk"], cat_estatus__descripcion="ACTIVO").order_by('id')
+		context['contacto'] = 'open'
+		context['direcciones'] = direcciones
 		return context
 
 
@@ -87,11 +102,31 @@ class EditarContactoView(UpdateView):
 
 	def form_valid(self, form):
 		form.instance.cat_estatus_id = 1
+		messages.success(self.request, "Se actualizó correctamente el contacto")
 		return super(EditarContactoView, self).form_valid(form)
 
 	def get_context_data(self, **kwargs):
 		context = super(EditarContactoView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		context['contacto'] = 'open'
+		context['editar'] = 'editar'
+		return context
+
+
+class ActivarContactoView(UpdateView):
+	model = Agenda
+	form_class = EditarContactoForm
+	success_url = reverse_lazy("lista_contactos_inactivos")
+	template_name = "agenda_update_form.html"
+
+	def form_valid(self, form):
+		form.instance.cat_estatus_id = 1
+		messages.success(self.request, "Se activó correctamente el contacto")
+		return super(ActivarContactoView, self).form_valid(form)
+
+	def get_context_data(self, **kwargs):
+		context = super(ActivarContactoView, self).get_context_data(**kwargs)
+		context['contacto'] = 'open'
+		context['activar'] = 'activar'
 		return context
 
 
@@ -103,9 +138,89 @@ class EliminarContactoView(UpdateView):
 
 	def form_valid(self, form):
 		form.instance.cat_estatus_id = 2
+		messages.success(self.request, "Se eliminó correctamente el contacto")
 		return super(EliminarContactoView, self).form_valid(form)
 
 	def get_context_data(self, **kwargs):
 		context = super(EliminarContactoView, self).get_context_data(**kwargs)
-		context['contacto'] = 'Active'
+		context['contacto'] = 'open'
+		context['eliminar'] = 'eliminar'
+		return context
+
+
+class ListaDireccionesView(ListView):
+	model = Direcciones
+	template_name = "list_direcciones.html"
+	queryset = Direcciones.objects.filter(cat_estatus__descripcion="ACTIVO").order_by('id')
+
+	def get_filter(self, queryset):
+		search = self.get_search()
+		if search:
+			queryset = queryset.filter(
+				Q(nombre__icontains=search)
+			)
+		return queryset
+
+	def get_context_data(self, **kwargs):
+		context = super(ListaDireccionesView, self).get_context_data(**kwargs)
+		context['contacto'] = 'open'
+		return context
+
+
+class AgregarDireccionContactoView(CreateView):
+	model = Direcciones
+	form_class = CrearDireccionContactoForm
+	template_name = "direccion_form.html"
+
+	def get_success_url(self):
+		contacto = Agenda.objects.get(pk=self.kwargs["pk"])
+		return reverse('detalle_contacto_nuevo', kwargs={'pk': contacto.id})
+
+	@transaction.atomic()
+	def form_valid(self, form):
+		form.instance.cat_estatus_id = 1
+		form.instance.agenda_id = self.kwargs["pk"]
+		messages.success(self.request, "Se agrego correctamente una dirección al contacto")
+		return super(AgregarDireccionContactoView, self).form_valid(form)
+
+	def get_context_data(self, **kwargs):
+		context = super(AgregarDireccionContactoView, self).get_context_data(**kwargs)
+		context['contacto'] = 'open'
+		return context
+
+
+class EditarDireccionContactoView(UpdateView):
+	model = Direcciones
+	form_class = EditarDireccionContactoForm
+	success_url = reverse_lazy("lista_contactos_nuevos")
+	template_name = "direccion_update_form.html"
+
+	@transaction.atomic()
+	def form_valid(self, form):
+		form.instance.cat_estatus_id = 1
+		messages.success(self.request, "Se actualizó correctamente la direccion del contacto")
+		return super(EditarDireccionContactoView, self).form_valid(form)
+
+	def get_context_data(self, **kwargs):
+		context = super(EditarDireccionContactoView, self).get_context_data(**kwargs)
+		context['contacto'] = 'open'
+		return context
+
+
+class EliminarDireccionContactoView(UpdateView):
+	model = Direcciones
+	form_class = EditarDireccionContactoForm
+	success_url = reverse_lazy("lista_contactos_nuevos")
+	template_name = "direccion_update_form.html"
+
+	@transaction.atomic()
+	def form_valid(self, form):
+		form.instance.cat_estatus_id = 2
+		messages.success(self.request, "Se eliminó correctamente la direccion del contacto")
+		return super(EliminarDireccionContactoView, self).form_valid(form)
+
+	def get_context_data(self, **kwargs):
+		context = super(EliminarDireccionContactoView, self).get_context_data(**kwargs)
+		context['contacto'] = 'open'
+		context['eliminar'] = 'eliminar'
 		return context
