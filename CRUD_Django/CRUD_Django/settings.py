4889b0541c28e0fe13061fa62602f2a6c006b66d
franciscohernandez <francisco.hdezreyes@gmail.com>
2015-07-23 09:52:57 -0500

    modificaciones en el settings.py

--- CRUD_Django/CRUD_Django/settings.py
+++ CRUD_Django/CRUD_Django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'CRUD',
         'USER': 'postgres',
-        'PASSWORD': 'alfredo',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
