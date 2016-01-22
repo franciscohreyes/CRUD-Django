aa7e3384da9d4b2f865a15dbbe900a94e5249320
Francisco Hernandez <francisco.hdezreyes@hotmail.com>
2016-01-21 21:58:14 -0600

    cambios en el archivo settings.py

--- CRUD_Django/principal/migrations/0001_initial.py
+++ CRUD_Django/principal/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(SchemaMigration):
             ('telefono', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('correo', self.gf('django.db.models.fields.EmailField')(max_length=100)),
             ('redes_sociales', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('fecha_registro', self.gf('django.db.models.fields.DateField')(blank=True, auto_now_add=True)),
+            ('fecha_registro', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
             ('cat_estatus', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['catalogos.Cat_estatus'])),
         ))
         db.send_create_signal('principal', ['Agenda'])
@@ -29,7 +29,7 @@ class Migration(SchemaMigration):
             ('calle', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('codigo_postal', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('tipo_direccion', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
-            ('fecha_registro', self.gf('django.db.models.fields.DateField')(blank=True, auto_now_add=True)),
+            ('fecha_registro', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
             ('agenda', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['principal.Agenda'])),
             ('cat_estatus', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['catalogos.Cat_estatus'])),
         ))
@@ -55,7 +55,7 @@ class Migration(SchemaMigration):
             'apellidos': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'cat_estatus': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['catalogos.Cat_estatus']"}),
             'correo': ('django.db.models.fields.EmailField', [], {'max_length': '100'}),
-            'fecha_registro': ('django.db.models.fields.DateField', [], {'blank': 'True', 'auto_now_add': 'True'}),
+            'fecha_registro': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'nombre': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'redes_sociales': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
@@ -68,7 +68,7 @@ class Migration(SchemaMigration):
             'cat_estatus': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['catalogos.Cat_estatus']"}),
             'ciudad': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'codigo_postal': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'fecha_registro': ('django.db.models.fields.DateField', [], {'blank': 'True', 'auto_now_add': 'True'}),
+            'fecha_registro': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'nombre': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'tipo_direccion': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'})
